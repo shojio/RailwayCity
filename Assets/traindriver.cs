64f9a7a1ff3236f64769e126539094fe0f3fe442
庄子嵩 <>
2018-05-29 20:34:57 +0900

    駅のホーム １つ目仮完成 加速スクリプトを書こうとしたら、キーを離したら止まっちゃうスクリプトになっちゃった

---
+++ Assets/traindriver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class traindriver : MonoBehaviour {
+	float speed = 0.01;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.X)) {
+			speed++;
+			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
