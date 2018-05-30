3105ab830a0dffe6b5583fb7d4f20d25c7ecfd53
庄子嵩 <>
2018-05-30 20:58:35 +0900

    色々試行錯誤したけどブレーキして止まれない（逆走し始める）。

---
+++ Assets/notchscript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class notchscript : MonoBehaviour {
+	public Text shownotch;
+	public int notch = 0;
+	public int brake = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (notch < -8) {
+			notch = -8;
+		}
+		if (notch > 5) {
+			notch = 5;
+		}
+		if (brake > 8) {
+			brake = 8;
+		}
+		if (brake < -5) {
+			brake = -5;
+		}
+		if (Input.GetKeyDown (KeyCode.X)) {
+			notch += 1;
+			brake -= 1;
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			notch = 0;
+			brake = 0;
+		}
+		if (Input.GetKeyDown (KeyCode.W)) {
+			notch -= 1;
+			brake += 1;
+		}
+			
+		if (notch == 0) {
+			shownotch.text = "切";
+		}
+		if (notch < 0) {
+			shownotch.text = brake.ToString ();
+			if (brake == 8) {
+				shownotch.text = "EB";
+			}
+		}
+		if (notch > 0) {
+			shownotch.text = notch.ToString ();
+		}
+	}
+}
--- Assets/traindriver.cs
+++ Assets/traindriver.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class traindriver : MonoBehaviour {
-	float speed = 0.01;
+	float speed = 0;
+	float power = 0;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.X)) {
-			speed++;
-			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+	void Update (){
+		
+		if (Input.GetKeyDown (KeyCode.X)) {
+			power += 0.001f;
 		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			power = 0.0000f;
+		}
+		if (Input.GetKeyDown (KeyCode.W)) {
+			power -= 0.005f;
+		}
+		if (power >= 0.005f) {
+			power = 0.005f;
+		}
+		if (power <= -0.04f) {
+			power = -0.04f;
+		}
+		if (speed == 0) {
+			transform.position += new Vector3 (0, 0, 0);
+		}
+		speed += power;
+		transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
 	}
 }
