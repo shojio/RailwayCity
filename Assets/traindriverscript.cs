10c448792c201ecd16e7ef3dfc6b7ff691845979
庄子嵩 <>
2018-06-26 20:30:39 +0900

    なんかやっぱり止まれない。。w 工夫してみよっと。

--- Assets/traindriverscript.cs
+++ Assets/traindriverscript.cs
@@ -1,57 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class traindriverscript : MonoBehaviour {
 	public float notch;
 	public Rigidbody rb;
-	public float position;
+	public float speed;
 	public float notchA;
-	public int brake;
-	public string shownotch;//ノッチ表示用
+	public Text shownotch;//ノッチ表示用
+	public Text showbrake;//ノッチ表示用
 
 	public float moveForceMultiplier;
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
+		rb = GameObject.Find("driver").GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//動かす
 		if (Input.GetKeyDown (KeyCode.X)) {
-			if (notch <= 4.5f) {
+			if ((notch <= 5) && (notch >= 0)) {
 				notch += 1;
 			}
 		}
-		if (notch >= -8.5f) {
-			if (Input.GetKeyDown (KeyCode.W)) {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			if ((notch <= 0) && (notch >= -8)) {
 				notch -= 1;
 			}
 		}
-		//ここから表示用
-		if ((notch <= 0) && (brake <= 8)) {
-			brake += 1;
-		}
-		if (notch == 0) {
-			shownotch = "切";
-		}
-		if (notch < 0) {
-			shownotch = brake.ToString ();
-			if (brake == 8) {
-				shownotch = "EB";
-			}
-		}
-		if (notch > 0) {
-			shownotch = notch.ToString ();
-		}
-		//ここまで表示用
-		position = transform.position.x;
-		Debug.Log (position);
-		notchA = 0.005f * notch;
-		Debug.Log (notch);
+		//動かす
 
+		//表示
+
+		if (notch == 0) {
+			shownotch.text = "切";
+			shownotch.ToString();
+		}
+		if (notch == -8) {
+			shownotch.text = "EB";
+			shownotch.ToString();
+		}
+		shownotch.text = notch.ToString ();
+		//表示
+
+		//スピードを測る
+		speed = rb.velocity.magnitude;
+		//スピードを測る
+
+
+
+		Debug.Log (notch);
+		notchA = notch * 0.025f;
 	}
 	void FixedUpdate() {
-		rb.AddForce (notchA, 0, 0, ForceMode.Force);
+		rb.AddForce (notchA * Vector3.right, ForceMode.Acceleration);
+		if (rb.velocity.x < 0) {
+			rb.velocity = new Vector3( 0, rb.velocity.y, rb.velocity.z);
+		}
 	}
 }
